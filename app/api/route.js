99881b6ed80cbb8d1f841e1962f2ca30bf7debea
qiewi <qiewifruit@gmail.com>
2024-09-03 15:51:03 +0700

    styles & public setup

--- app/api/route.js
+++
@@ -1,9 +0,0 @@
-export async function GET(req) {
-    const users = [
-        {id: 1, name: 'John Doe'},
-        {id: 2, name: 'Jane Doe'},
-        {id: 3, name: 'John Smith'}
-    ];
-
-    return new Response(JSON.stringify(users))
-}
--- app/posts/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>POST</div>
-  )
-}
-
-export default page
