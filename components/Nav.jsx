d0d5735b1e813990cbaf0609dc9b474778895229
qiewi <qiewifruit@gmail.com>
2024-09-03 17:47:33 +0700

    dropdown for mobile navs

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,7 +9,8 @@ const Nav = () => {
     const isUserLoggedIn = true;
 
     const [providers, setProviders] = useState(null);
-
+    const [toggleDropdown, setToggleDropdown] = useState(false);
+    
     useEffect(() => {
         const setProviders = async () => {
             const response = await getProviders();
@@ -86,8 +87,38 @@ const Nav = () => {
                             height={37}
                             className="rounded-full"
                             alt="profile"
-                            onClick={() => {}}
+                            onClick={() => setToggleDropdown((prev) => !prev)}
                         />
+
+                        {toggleDropdown && (
+                            <div className="dropdown">
+                                <Link
+                                    href="/profile"
+                                    className="dropdown_link"
+                                    onClick={() => setToggleDropdown(false)}
+                                >
+                                    My Profile
+                                </Link>
+
+                                <Link
+                                    href="/create-prompt"
+                                    className="dropdown_link"
+                                    onClick={() => setToggleDropdown(false)}
+                                >
+                                    Create Prompt
+                                </Link>
+                                <button
+                                    type="button"
+                                    onClick={() => {
+                                        setToggleDropdown(false);
+                                        signOut();
+                                    }}
+                                    className="mt-5 w-full black_btn"
+                                >
+                                    Sign Out
+                                </button>
+                            </div>
+                        )}
                     </div>
                 ) : (
                     <>
