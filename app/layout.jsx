52533b7d88baaf59cfddd33aae16052b88bc686c
qiewi <qiewifruit@gmail.com>
2024-09-03 16:03:35 +0700

    finished page layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,7 @@ export const metadata = {
     description: 'Discover & Share AI Prompts'
 }
 
-const RootLayout = () => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
         <body>
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div>page</div>
+    <div>Home</div>
   )
 }
 
