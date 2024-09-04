774be9665e5b49c61422bff6bf2d845cc6074914
qiewi <105619444+qiewi@users.noreply.github.com>
2024-09-04 17:31:54 +0700

    session configuration

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -12,7 +12,13 @@ const handler = NextAuth({
         })
     ],
     async session({ session }) {
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
 
+        session.user.id = sessionUser._id.toString();
+
+        return session;
     },
     async signIn({ profile }) {
         try {
